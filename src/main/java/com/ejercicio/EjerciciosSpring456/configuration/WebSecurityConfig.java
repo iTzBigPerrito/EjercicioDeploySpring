c1600fe3b04c8074823381fb2369a41543ed383d
Daniel Tamayo <byrdanto@gmail.com>
2022-09-14 17:35:48 -0500

    Ejercicio Deploy de OB en Spring

---
+++ src/main/java/com/ejercicio/EjerciciosSpring456/configuration/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.ejercicio.EjerciciosSpring456.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalAuthentication
+public class WebSecurityConfig {
+
+    /**
+     * Configuración de seguridad Web para URL's
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/laptops").permitAll()
+                .antMatchers("/api/laptops/{id}").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and().formLogin()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+    /**
+     * Configuración de usuarios para la autenticación
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
